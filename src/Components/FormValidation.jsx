3cccdeb1d959141c8a2bd22e035443b963a9f055
Muhammad Rooman <56633412+muhammadRooman@users.noreply.github.com>
2022-11-04 22:00:15 +0500

    Form Validation using state

--- src/Components/FormValidation.jsx
+++ src/Components/FormValidation.jsx
@@ -2,7 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap-v5";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import { validations } from "./indexJavaScript";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { validations } from "./indexJavaScript"; //import Validation function form indexJavaScript
 
 const FormValidation = () => {
   const [error, setError] = useState({
@@ -10,9 +12,14 @@ const FormValidation = () => {
     lname: "",
     email: "",
     phone: "",
+    date: "",
+    month: "",
+    year: "",
     password: "",
+
     conformPassword: "",
   });
+  console.log("error", error);
 
   const [formData, setFormData] = useState([]);
   const [data, setData] = useState({
@@ -22,6 +29,9 @@ const FormValidation = () => {
     phone: "",
     password: "",
     conformPassword: "",
+    date: "",
+    month: "",
+    year: "",
   });
 
   const loginhandle = (e) => {
@@ -39,7 +49,7 @@ const FormValidation = () => {
   useEffect(() => {
     setFormData(data);
   });
-  console.log(error);
+  console.log("ALL DATA IS HERE", data);
 
   return (
     <div className="mt-5">
@@ -64,6 +74,9 @@ const FormValidation = () => {
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Last Name</Form.Label>
             <Form.Control
+              className={
+                error.lname ? "boder" : !data.lname ? "" : "bordergreen"
+              }
               type="lname"
               placeholder="Last Name"
               name="lname"
@@ -75,6 +88,9 @@ const FormValidation = () => {
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Email</Form.Label>
             <Form.Control
+              className={
+                error.email ? "boder" : !data.email ? "" : "bordergreen"
+              }
               type="email"
               placeholder="Email"
               name="email"
@@ -86,6 +102,9 @@ const FormValidation = () => {
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Phone</Form.Label>
             <Form.Control
+              className={
+                error.phone ? "boder" : !data.phone ? "" : "bordergreen"
+              }
               type="phone"
               placeholder="Phone"
               name="phone"
@@ -94,10 +113,146 @@ const FormValidation = () => {
             />{" "}
             <Form.Text className="text errorCss">{error.phone}</Form.Text>
           </Form.Group>
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          {/* Age   */}
+
+          <Form.Label>Age</Form.Label>
+          <Form.Group>
+            <Row>
+              <Col md={2}>
+                <select
+                  className={`Select ${
+                    error.date ? "boder" : !data.date ? "" : "bordergreen"
+                  }`}
+                  onChange={(e) => {
+                    data.date = e.target.value;
+                  }}
+                >
+                  <option disabled selected value="" id="options" hidden>
+                    Date
+                  </option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                  <option value="4">4</option>
+                  <option value="5">5</option>
+                  <option value="6">6</option>
+                  <option value="7">7</option>
+                  <option value="8">8</option>
+                  <option value="9">9</option>
+                  <option value="10">10</option>
+                  <option value="11">11</option>
+                  <option value="12">12</option>
+                  <option value="13">13</option>
+                  <option value="14">14</option>
+                  <option value="15">15</option>
+                  <option value="16">16</option>
+                  <option value="17">17</option>
+                  <option value="18">18</option>
+                  <option value="19">19</option>
+                  <option value="20">20</option>
+                  <option value="21">21</option>
+                  <option value="22">22</option>
+                  <option value="23">23</option>
+                  <option value="24">24</option>
+                  <option value="25">25</option>
+                  <option value="26">26</option>
+                  <option value="27">27</option>
+                  <option value="28">28</option>
+                  <option value="29">29</option>
+                  <option value="30">30</option>
+                  <option value="31">31</option>
+                </select>
+                <Form.Text className="text errorCss">{error.date}</Form.Text>
+              </Col>
+              <Col md={2}>
+                <select
+                  className={`Select ${
+                    error.month ? "boder" : !data.month ? "" : "bordergreen"
+                  }`}
+                  onChange={(e) => {
+                    data.month = e.target.value;
+                  }}
+                >
+                  <option selected value="" hidden disabled>
+                    Month
+                  </option>
+                  <option value="January">January</option>
+                  <option value="February">February</option>
+                  <option value="March">March</option>
+                  <option value="April">April</option>
+                  <option value="May">May</option>
+                  <option value="June">June</option>
+                  <option value="July">July</option>
+                  <option value="August">August</option>
+                  <option value="September">September</option>
+                  <option value="October">October</option>
+                  <option value="November">November</option>
+                  <option value="December">December</option>
+                </select>
+                <Form.Text className="text errorCss">{error.month}</Form.Text>
+              </Col>
+              <Col md={2}>
+                <select
+                  className={`Select ${
+                    error.year ? "boder" : !data.year ? "" : "bordergreen"
+                  }`}
+                  onChange={(e) => {
+                    data.year = e.target.value;
+                  }}
+                >
+                  <option selected disabled value="" hidden>
+                    Year
+                  </option>
+
+                  <option value="2024">{2024}</option>
+                  <option value="2023">2023</option>
+                  <option value="2022">2022</option>
+                  <option value="2021">2021</option>
+                  <option value="2020">2020</option>
+                  <option value="2019">2019</option>
+                  <option value="2018">2018</option>
+                  <option value="2017">2017</option>
+                  <option value="2016">2016</option>
+                  <option value="2015">2015</option>
+                  <option value="2014">2014</option>
+                  <option value="2013">2013</option>
+                  <option value="2012">2012</option>
+                  <option value="2011">2011</option>
+                  <option value="2010">2010</option>
+                  <option value="2009">2009</option>
+                  <option value="2008">2008</option>
+                  <option value="2007">2007</option>
+                  <option value="2006">2006</option>
+                  <option value="2005">2005</option>
+                  <option value="2004">2004</option>
+                  <option value="2003">2003</option>
+                  <option value="2002">2002</option>
+                  <option value="2001">2001</option>
+                  <option value="2000">2000</option>
+                  <option value="1999">1999</option>
+                  <option value="1998">1998</option>
+                  <option value="1997">1997</option>
+                  <option value="1996">1996</option>
+                  <option value="1995">1995</option>
+                  <option value="1994">1994</option>
+                  <option value="1993">1993</option>
+                  <option value="1992">1992</option>
+                  <option value="1991">1991</option>
+                  <option value="1990">1990</option>
+                  <option value="1989">1989</option>
+                </select>
+                <Form.Text className="text errorCss">{error.year}</Form.Text>
+              </Col>
+            </Row>
+          </Form.Group>
+
+          <Form.Group className="mb-3 mt-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control
-              type="password"
+              className={
+                error.password ? "boder" : !data.password ? "" : "bordergreen"
+              }
+              type="text"
               placeholder="Password"
               name="password"
               value={data.password}
@@ -108,7 +263,14 @@ const FormValidation = () => {
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Conform Password</Form.Label>
             <Form.Control
-              type="conformPassword"
+              className={
+                error.conformPassword
+                  ? "boder"
+                  : !data.conformPassword
+                  ? ""
+                  : "bordergreen"
+              }
+              type="text"
               placeholder="Conform Password"
               name="conformPassword"
               value={data.conformPassword}
--- src/Components/indexJavaScript.js
+++ src/Components/indexJavaScript.js
@@ -1,3 +1,4 @@
+//Regex Method
 const userNameCheck = /^[A-Z-a-z. ]{3,15}$/;
 const lastNameCheck = /^[A-Z-a-z. ]{3,15}$/;
 const emailCheck = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
@@ -5,21 +6,21 @@ const phoneCheck = /^[0-9-+]{11,14}$/;
 const passwordCheck = /^[1-9-a-z-A-Z-!@#$%^&*()_"<?]{8,15}$/;
 const conformPasswordCheck = /^[1-9-a-z-A-Z-!@#$%^&*()_"<?]{8,15}$/;
 
+//Function :: (data) is parameter comes in formValidation Components ::: setError({ ...validations(data) });
 export const validations = (data) => {
   const errors = {};
 
-  //name is empty logic
+  //Name is empty logic
   if (data.names == "") {
     console.log("Name is Empty");
     errors.names = "Name is Empty";
   } else if (!userNameCheck.test(data.names)) {
-      errors.names = "Alphabet mint 3 max 15";
-   
+    errors.names = "Alphabet mint 3 max 15";
   } else {
     errors.names = "";
     console.log("name is not Empty");
   }
-  //last name is empty logic
+  //Last name is empty logic
   if (data.lname == "") {
     console.log("Last Name is Empty");
     errors.lname = "Last Name is Empty";
@@ -30,7 +31,7 @@ export const validations = (data) => {
     console.log("last Name is not Empty");
   }
 
-  //email is empty logic
+  //Email is empty logic
   if (data.email == "") {
     console.log("Email is Empty");
     errors.email = "Email is Empty";
@@ -40,7 +41,7 @@ export const validations = (data) => {
     errors.email = "";
     console.log("Email is not Empty");
   }
-  //phone is empty logic
+  //Phone is empty logic
   if (data.phone == "") {
     console.log("Phone is Empty");
     errors.phone = "Phone is Empty";
@@ -50,6 +51,63 @@ export const validations = (data) => {
     errors.phone = "";
     console.log("Phone is not Empty");
   }
+
+  //Age ::: Date of Birth
+  if (!data.date || !data.month || !data.year) {
+    console.log("All age is empty");
+    errors.date = "Date is Empty";
+    errors.month = "Month is Empty";
+    errors.year = "Year is Empty";
+  }
+
+  if (data.date) {
+    errors.date = "";
+  }
+  if (data.month) {
+    errors.month = "";
+  }
+  if (data.year) {
+    errors.year = "";
+  }
+
+  if (
+    (data.month == "January" && data.date <= 31) ||
+    (data.month == "March" && data.date <= 31) ||
+    (data.month == "May" && data.date <= 31) ||
+    (data.month == "July" && data.date <= 31) ||
+    (data.month == "August" && data.date <= 31) ||
+    (data.month == "October" && data.date <= 31) ||
+    (data.month == "December" && data.date <= 31)
+  ) {
+    console.log("yes month is 31");
+    errors.month = "";
+  } else if (
+    (data.month == "February" && data.date < 30) ||
+    (data.month == "April" && data.date < 31) ||
+    (data.month == "June" && data.date < 31) ||
+    (data.month == "September" && data.date < 31) ||
+    (data.month == "November" && data.date < 31)
+  ) {
+    console.log("yes 30");
+    errors.month = "";
+  } else {
+    console.log("yes month is not 31");
+  }
+
+  if (
+    (data.month == "February" && 0 == data.year % 4 && 0 != data.year % 100) ||
+    (0 == data.year % 400 && data.date == 29)
+  ) {
+    console.log("i am 29");
+  } else if (data.month === "February" && data.date == 28) {
+    console.log("yes i am 28");
+  } else if (data.month === "February" && data.date > 28) {
+    console.log("i am gartr 28");
+    errors.month =
+      "28 or 29 days in February,this date is not Available in this  year";
+    errors.year = "This Month is not Available in this Year ";
+  }
+
   //password is empty logic
   if (data.password == "") {
     console.log("Password is Empty");
@@ -60,15 +118,26 @@ export const validations = (data) => {
     errors.password = "";
     console.log("Password is not Empty");
   }
+
   //conform Password is empty logic
   if (data.conformPassword == "") {
     console.log("conform Password is Empty");
     errors.conformPassword = "Conform Password is Empty";
   } else if (!conformPasswordCheck.test(data.conformPassword)) {
     errors.conformPassword = "Invalid Conform Password min 8 ,max 15 ";
-  } else {
-    errors.conformPassword = "";
-    console.log("Conform Password is not Empty");
+  }
+
+  //Password and Conform Password is match ::: Logic
+  if (data.password && data.conformPassword) {
+    if (data.password === data.conformPassword) {
+      errors.conformPassword = "";
+      errors.password = "";
+      console.log("match password");
+    } else {
+      errors.conformPassword = "Not Match";
+      errors.password = "Not Match";
+      console.log("Conform Password is not Empty");
+    }
   }
 
   return errors;
